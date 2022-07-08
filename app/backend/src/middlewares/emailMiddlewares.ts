774ed22114be0ca4fb1109c39caaf9dc70e4a3a7
Diego Dmitry <diegodmitry@hotmail.com>
2022-07-08 16:25:22 -0300

    feat: added emailMiddlewares.ts file. Req 5 e 9.

---
+++ app/backend/src/middlewares/emailMiddlewares.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+
+function verifyMail(mail: string):boolean {
+  const regexMail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  return regexMail.test(mail);
+}
+
+enum StatusCodes {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+}
+
+// Req 5 e 9
+const emailMiddleware = (req:Request, res:Response, next: NextFunction) => {
+  const { email } = req.body;
+  if (email) {
+    if (verifyMail(email)) {
+      return next();
+    } return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Incorrect email or password' });
+  } return res.status(StatusCodes.BAD_REQUEST).json({ message: 'All fields must be filled' });
+};
+
+export default emailMiddleware;
