4decb2a192a40eaa7091ee485adfcaf4b0b30564
Diego Dmitry <diegodmitry@hotmail.com>
2022-07-09 15:05:24 -0300

    fix: trying to fix Req 3.

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -20,6 +20,6 @@ export const loginValidateController = async (req:Request, res:Response) => {
   const { authorization } = req.headers;
   if (authorization !== undefined) {
     const role = await getRole(authorization);
-    return res.status(200).json({ "role": role });
+    return res.status(200).json({ role });
   } return res.status(401).json({ message: 'Token inválido' });
 };
