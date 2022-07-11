3559002e7ea6162107ba374ad1b1cbd532e7c053
Diego Dmitry <diegodmitry@hotmail.com>
2022-07-11 15:46:46 -0300

    feat: set up matches migration.

---
+++ app/backend/src/database/migrations/99999999999999-03-create-matches.js
@@ -0,0 +1,39 @@
+
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        field: 'home_team',
+        references: { model: 'teams', key: 'id' },
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER,
+        field: 'away_team',
+        references: { model: 'teams', key: 'id' },
+      },
+      awayTeamGoals: {
+        field: 'away_team_goals',
+        type: Sequelize.INTEGER
+      },
+      inProgress: {
+        field: 'in_progress',
+        type: Sequelize.BOOLEAN
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  }
+};
