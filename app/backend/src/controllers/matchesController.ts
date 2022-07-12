f18eab625123537ed3c14b38fa4e7e0d92fdd71e
Diego Dmitry <diegodmitry@hotmail.com>
2022-07-11 22:21:28 -0300

    feat: added createMatches and updateStatusInProgress middlewares in matchesController file.

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -1,13 +1,34 @@
 import { Request, Response } from 'express';
-import getAll from '../services/matchesService';
+import matchesService from '../services/matchesService';
 
 const getMatches = async (req:Request, res:Response) => {
   try {
-    const matches = await getAll();
+    const matches = await matchesService.getAll();
     return res.status(200).json(matches);
   } catch (error) {
     console.log(error);
   }
 };
 
-export default getMatches;
+// Req 23
+const createMatches = async (req:Request, res:Response) => {
+  try {
+    const match = await matchesService.create(req.body);
+    return res.status(201).json(match);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Req 24
+const updateStatusInProgress = async (req:Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const response = await matchesService.updateStatusInProgress(id);
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default { getMatches, createMatches, updateStatusInProgress };
